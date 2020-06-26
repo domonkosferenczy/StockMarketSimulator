6a263095132e3d1449ec694dbd67cc43513b17db
domonkosferenczy <ferenczy.domonkos@gmail.com>
2020-06-26 11:20:05 +0200

    Stocks can be added during running

--- src/components/Global_Components/AllStocks.js
+++ src/components/Global_Components/AllStocks.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
-import { requestTickers, requestAll } from "../../api/parseData";
+import React, { useState, useContext, useEffect } from "react";
+import { DataContext } from "../../Store/Data";
+import { StoreContext } from "../../Store/Store";
+import { requestTickers, requestStock } from "../../api/parseData";
 
-function AllStocks() {
-  const [localState, setlocalState] = useState(undefined);
+function AllStocks(props) {
+  const [data, DataDispatch] = useContext(DataContext);
+  const [state, StateDispatch] = useContext(StoreContext);
+
+  const inititalState = {
+    tickers: {},
+    shown: false,
+    loaded: false,
+  };
+
+  const [localState, setlocalState] = useState(inititalState);
+
+  useEffect(() => {
+    setlocalState((prevState) => {
+      return {
+        ...prevState,
+        shown: props.show,
+      };
+    });
+  }, [props.show]);
 
   const requestData = async () => {
     const all = await requestTickers();
@@ -10,17 +30,25 @@ function AllStocks() {
       tickers: {},
     };
     Object.keys(all.tickers).forEach((ticker) => {
-      const obj = {
-        [ticker]: {
-          selected: false,
-          startDate: all.tickers[ticker].startDate,
-          endDate: all.tickers[ticker].endDate,
-          name: all.tickers[ticker].name,
-        },
-      };
-      Object.assign(newData.tickers, obj);
+      if (!Object.keys(data.stocks).includes(ticker)) {
+        const obj = {
+          [ticker]: {
+            selected: false,
+            startDate: all.tickers[ticker].startDate,
+            endDate: all.tickers[ticker].endDate,
+            name: all.tickers[ticker].name,
+          },
+        };
+        Object.assign(newData.tickers, obj);
+      }
     });
-    setlocalState(newData);
+    setlocalState((prevState) => {
+      return {
+        ...prevState,
+        tickers: newData.tickers,
+        loaded: true,
+      };
+    });
   };
 
   const selectHandler = (ticker) => {
@@ -38,39 +66,73 @@ function AllStocks() {
     });
   };
 
-  if (localState === undefined) {
-    requestData();
-    return "Loading...";
+  const closeWindow = () => {
+    setlocalState((prevState) => {
+      return {
+        ...prevState,
+        shown: false,
+      };
+    });
+    props.hide();
+  };
+
+  const add = async () => {
+    const toAdd = Object.keys(localState.tickers).filter(
+      (ticker) => localState.tickers[ticker].selected
+    );
+    const prevData = data;
+    for (let i = 0; i < toAdd.length; i++) {
+      const data = await requestStock(toAdd[i]);
+      Object.assign(prevData.stocks, data.stocks);
+    }
+    DataDispatch({ type: "SET_DATA", payload: prevData });
+    StateDispatch({ type: "CLEAR_HISTORY" });
+    closeWindow();
+  };
+
+  let visibleClass = "popup-window-toHide";
+  if (!localState.shown) {
+    if (!localState.loaded) {
+      requestData();
+    }
+    return "";
   } else {
     const tickers = [];
+    if (localState.loaded) {
+      props.loaded();
+      visibleClass = "popup-window-toShow";
+    }
 
     Object.keys(localState.tickers).map((ticker) => {
-      tickers.push(
-        <div
-          className="popup-window-table-body"
-          key={`${ticker}-allStocks`}
-          onClick={() => selectHandler(ticker)}
-        >
-          <span>{localState.tickers[ticker].name}</span>
-          <span>{ticker.toUpperCase()}</span>
-          <span>{localState.tickers[ticker].startDate}</span>
-          <span>{localState.tickers[ticker].endDate}</span>
-          <span>
-            <input
-              type="checkbox"
-              name="allStocks"
-              value={ticker.toUpperCase()}
-              checked={localState.tickers[ticker].selected}
-              onChange={() => selectHandler(ticker)}
-            />
-          </span>
-        </div>
-      );
+      if (!Object.keys(data.stocks).includes(ticker)) {
+        tickers.push(
+          <div
+            className="popup-window-table-body"
+            key={`${ticker}-allStocks`}
+            onClick={() => selectHandler(ticker)}
+          >
+            <span>{localState.tickers[ticker].name}</span>
+            <span>{ticker.toUpperCase()}</span>
+            <span>{localState.tickers[ticker].startDate}</span>
+            <span>{localState.tickers[ticker].endDate}</span>
+            <span>
+              <input
+                type="checkbox"
+                name="allStocks"
+                value={ticker.toUpperCase()}
+                checked={localState.tickers[ticker].selected}
+                onChange={() => selectHandler(ticker)}
+              />
+            </span>
+          </div>
+        );
+      }
     });
 
     return (
-      <div className="popup-window-bg">
-        <div className="popup-window popup-window-toShow">
+      <div className={`popup-window-bg ${visibleClass}`}>
+        <div className="popup-close" onClick={closeWindow}></div>
+        <div className="popup-window">
           <div className="popup-window-table">
             <div className="popup-window-table-header">
               <span>Name</span>
@@ -79,11 +141,13 @@ function AllStocks() {
               <span>To</span>
               <span>Select</span>
             </div>
-            {tickers}
+            {tickers.length === 0 ? "No More Stocks Available" : tickers}
           </div>
           <div className="popup-window-table-footer">
             <div>
-              <button className="Dashboard-button">Add them</button>
+              <button className="Dashboard-button" onClick={add}>
+                Add them
+              </button>
             </div>
           </div>
         </div>
--- src/components/Sidebar/AvailableStocks.js
+++ src/components/Sidebar/AvailableStocks.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import StocksFilter from "./StocksFilter";
 import StocksList from "./StocksList";
+import { StoreContext } from "../../Store/Store";
 import AllStocks from "components/Global_Components/AllStocks";
 
 function AvailableStocks() {
+  const [state, stateDispatch] = useContext(StoreContext);
+
+  const [showAllStocks, setShowAllStocks] = useState(false);
+  const [loadedAllStocks, setLoadedAllStocks] = useState(true);
+
+  const showAllStocksHandler = () => {
+    setShowAllStocks(true);
+    setLoadedAllStocks(false);
+    stateDispatch({ type: "SET_PAUSED", payload: true });
+  };
+
+  const hideAllStocks = () => {
+    setShowAllStocks(false);
+  };
+
+  const loadedAllStocksHandler = () => {
+    setLoadedAllStocks(true);
+  };
+
   return (
     <div className="AvailableStocks">
       <div className="AvailableStocks-header">
         <span>AVAILABLE STOCKS</span>
-        <button className="Dashboard-button">NEW STOCK</button>
+        <button className="Dashboard-button" onClick={showAllStocksHandler}>
+          {loadedAllStocks ? "NEW STOCK" : "LOADING..."}
+        </button>
       </div>
-      <AllStocks />
+      <AllStocks
+        show={showAllStocks}
+        hide={hideAllStocks}
+        loaded={loadedAllStocksHandler}
+      />
       <StocksFilter />
       <StocksList />
     </div>
